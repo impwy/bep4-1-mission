285338edbd1527e7cf0821051aa2cf777110ca54
yong <pwy6817@gmail.com>
2026-09-17 12:33:54 +0900

    [박원용/완료] 0002

--- src/test/java/com/back/BackApplicationTests.java
+++ src/test/java/com/back/BackApplicationTests.java
@@ -1,13 +1,47 @@
 package com.back;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.back.initData.DataInit;
+import com.back.repository.MemberRepository;
+import com.back.repository.PostRepository;
+
+@SpringBootTest(properties = {
+        "spring.datasource.url=jdbc:h2:mem:init_test;MODE=MySQL;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+        "spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class BackApplicationTests {
+    @Autowired DataInit dataInit;
+    @Autowired MemberRepository memberRepository;
+    @Autowired PostRepository postRepository;
 
     @Test
-    void contextLoads() {
+    void initializationCanRunAgainWithoutDuplicates() {
+        dataInit.makeBaseMembers();
+        dataInit.makeBasePosts();
+
+        assertThat(memberRepository.count()).isEqualTo(3);
+        assertThat(postRepository.count()).isEqualTo(6);
+        assertThat(memberRepository.findByUsername("user1")).isPresent();
+    }
+
+    @Test
+    @Transactional
+    void initializationFillsMissingMembers() {
+        postRepository.deleteAll();
+        memberRepository.deleteAll();
+        memberRepository.saveAndFlush(new com.back.entity.Member("user2", "existing-password", "existing"));
+
+        dataInit.makeBaseMembers();
+        dataInit.makeBasePosts();
+
+        assertThat(memberRepository.count()).isEqualTo(3);
+        assertThat(postRepository.count()).isEqualTo(6);
     }
 
 }
