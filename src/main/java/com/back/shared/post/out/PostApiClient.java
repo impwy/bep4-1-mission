0eb02b2f968290ff703cca3c5abe74a2f6e64bcc
yong <pwy6817@gmail.com>
2026-09-22 11:49:02 +0900

    각종 설정들을 application.yml 파일에 모아두기

--- src/main/java/com/back/shared/post/out/PostApiClient.java
+++ src/main/java/com/back/shared/post/out/PostApiClient.java
@@ -2,6 +2,7 @@ package com.back.shared.post.out;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
@@ -10,9 +11,13 @@ import com.back.shared.post.dto.PostDto;
 
 @Service
 public class PostApiClient {
-    private final RestClient restClient = RestClient.builder()
-                                                    .baseUrl("http://localhost:8080/api/v1/post")
-                                                    .build();
+    private final RestClient restClient;
+
+    public PostApiClient(@Value("${custom.global.internalBackUrl}") String internalBackUrl) {
+        this.restClient = RestClient.builder()
+                                    .baseUrl(internalBackUrl + "/api/v1/post")
+                                    .build();
+    }
 
     public List<PostDto> getItems() {
         return restClient.get()
